3de09f54a37ca523b366dd0a04ffad0cc47fd751
kittiphiphat <makuth824@gmail.com>
2023-10-14 18:38:09 +0700

    web

--- src/components/NavbarPage.vue
+++ src/components/NavbarPage.vue
@@ -13,7 +13,8 @@
   
     
           <div class="col-md-3 text-end" style=" top: 1rem; margin-right: 2rem;">
-            <a href="/loginPage"><button  class="btn btn-outline-info link-light me-3" style="padding: 0.3rem;" >เข้าสู่ระบบ</button></a>
+            
+            <a href="/loginPage"><button  class="btn btn-outline-info link-light me-3" style="padding: 0.3rem;" ><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" style="fill: rgb(255, 255, 255);transform: ;msFilter:;"><path d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"></path></svg></button></a>
           </div>
         </header>
         <!-- Navbar -->
